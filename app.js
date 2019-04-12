3316adc24109945ef3a8a829fb892d62e81d3b0d
LP <591094678@qq.com>
2019-04-12 14:00:25 +0800

    asd

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express=require('express');
 const bodyparser=require('body-parser');
 const content=require('./routes/content.js');
 var server=express();
-server.listen(3000);
+server.listen(5050);
 server.use(express.static('./public'));
 server.use(bodyparser.urlencoded({
 	extended:false
